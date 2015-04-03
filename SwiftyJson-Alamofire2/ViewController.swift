4096d0f27d98380bda04fc64966d10050f0552de
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2015-04-03 10:05:02 +0900

    初期コミット

--- SwiftyJson-Alamofire2/ViewController.swift
+++ SwiftyJson-Alamofire2/ViewController.swift
@@ -7,19 +7,30 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func requestData(){
+        Alamofire.request(.GET, "http://httpbin.org/get")
+            .responseJSON { (request, response, JSON, error) in
+                println(JSON)
+            }
+            .responseString { (request, response, string, error) in
+                println(string)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
